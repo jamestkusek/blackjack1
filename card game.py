2d370ccb74a054d452cf37a02e57e9b0ad930e63
jamestkusek <90416554+jamestkusek@users.noreply.github.com>
2021-10-14 19:08:12 +0100

    finishing touches

--- card game.py
+++ card game.py
@@ -2,15 +2,27 @@ import random
 import tkinter as tk
 from PIL import Image, ImageTk
 import time
+import tkinter.font as tf
 cards = []
 suits = ["Hearts","Diamonds","Spades","Clubs"]
 picturecards = ["Ace","Jack","Queen","King"]
-playernum = 5
+playernumber = (input("How many Players?"))
+
+while playernumber not in ["2", "3", "4", "5", "6", "7", "8"]:
+    playernumber = (input("How many Players?"))
+
+playernum = int(playernumber)
 playerdecks = []
 masterwindow = tk.Tk()
-masterwindow.geometry("825x600")
-mastercanvas = tk.Canvas(width=825, height = 1000)
+geometrysize = str(str((playernum * 165)) +"x600")         
+masterwindow.geometry(geometrysize)
+masterwindow.resizable(0,0)
+mastercanvas = tk.Canvas(width = playernum * 165, height = 1000)
 mastercanvas.pack()
+exitbutton = tk.Button(text = "Exit.", height = 1, width = 3,bg = "RED",command = masterwindow.destroy)
+exitbutton.place(x = 125 +  165* (playernum-1), y = 10)
+title = mastercanvas.create_text(100, 20, font = ("Gothic",30), text="Blackjack", )
+mastercanvas.update
 global cardref
 cardref = []
 global status
@@ -20,6 +32,7 @@ scorescounter = []
 for i in range(playernum):
     cardref.append([])
     status.append([])
+
 def setup():
     
     global cardcounter
@@ -66,8 +79,8 @@ def setup():
         for y in x:
             carddisplay = tk.Label(masterwindow)
             cardname = (y[0] + y[1] + ".png")
-            card = Image.open(str("/home/jtk/Python Things/assets/" + cardname))
-            card = card.resize((75,100))
+            card = Image.open(str("assets/" + cardname))
+            card = card.resize((65,86))
             card = ImageTk.PhotoImage(card)
             carddisplay.photo = card
             cardimage = mastercanvas.create_image(50 + (165* cardimages.index(x)) + coordinatecount,200, image = card)
@@ -91,8 +104,8 @@ def gameround():
             else:
                 global proceed
                 proceed = tk.IntVar()
-                decision1 = tk.Button(text = "Stick", height = 1, width = 1, command = stick)
-                decision2 = tk.Button(text = "Hit", height = 1, width = 1, command = hit)
+                decision1 = tk.Button(text = "Stick", height = 1, width = 3, command = stick)
+                decision2 = tk.Button(text = "Hit", height = 1, width = 3, command = hit)
                 decision1.place(x = 22 + (150* playerposition) , y = 290)
                 decision2.place(x= 57 + (150* playerposition) , y = 290)
                 mastercanvas.update()
@@ -132,9 +145,8 @@ def hit():
                 
 
             if (playerscore + (1 * acecount)) > 21:
-                print("Player " + str(playerposition + 1) + " has gone bust.")
                 bust = tk.Label(masterwindow)
-                cross = Image.open(str("/home/jtk/Python Things/assets/cross.png"))
+                cross = Image.open(str("assets/cross.png"))
                 cross = cross.resize((70,70))
                 cross = ImageTk.PhotoImage(cross)
                 bust.photo = cross
@@ -163,8 +175,8 @@ def cardaddprocedure():
         cardname = (str(newcard[1]) + str(newcard[0][0]) + ".png")
     
     cardadd = tk.Label(masterwindow)
-    card2 = Image.open(str("/home/jtk/Python Things/assets/" + cardname))
-    card2 = card2.resize((75,100))
+    card2 = Image.open(str("assets/" + cardname))
+    card2 = card2.resize((65,86))
     card2 = ImageTk.PhotoImage(card2)
     cardadd.photo = card2
     card = mastercanvas.create_image(50 + (165*int(playerposition)) + (25*(len(cardcounter[playerposition])+ 1)),200, image = card2)
@@ -173,8 +185,6 @@ def cardaddprocedure():
   
 
 def endscore():
-    global acecount
-    global playerscore
     for x in playerdecks:
         playerposition = playerdecks.index(x)
         acecount = 0
@@ -188,58 +198,89 @@ def endscore():
                     acecount += 1
                 else:
                     playerscore += 10
+
+        
         if acecount > 0:
-            acedecision()
-            scorescounter.append(playerscore)
+            scoreafterace = acedecision(playerscore, acecount)
+            scorescounter.append(scoreafterace)
+            
         else:
             scorescounter.append(playerscore)
 
     
     for i in range(0, playernum):
         if scorescounter[i] > 21:
-            mastercanvas.create_text(150,(350 + (i * 20)),text=("Player "+str(i+1)+" went bust at "+str(scorescounter[i])+"."))
+            mastercanvas.create_text(150,(350 + (i * 20)),justify = "left",text=("Player "+str(i+1)+" went bust at "+str(scorescounter[i])+"."))
         else:
-            mastercanvas.create_text(150,(350 + (i * 20)),text=("Player "+str(i+1)+" finishes with a score of "+str(scorescounter[i])+"."))
+            mastercanvas.create_text(150,(350 + (i * 20)),justify = "left",text=("Player "+str(i+1)+" finishes with a score of "+str(int(scorescounter[i]))+"."))
             mastercanvas.update()
             time.sleep(0.1)
+    finallist = []
     for x in scorescounter:
-        position = scorescounter.index(x)
         if x > 21:
-            scorescounter[position] == 0
-            print(scorescounter[position])
+            finallist.append(0)
+        else:
+            finallist.append(x)
             
-    winningplayer = max(scorescounter)
-    mastercanvas.create_text(150,(350 + (playernum* 30)),text=("Player "+ str(int(scorescounter.index(winningplayer))+1)+" WINS with a score of "+str(max(scorescounter))))
-    mastercanvas.update()
-    
-
-def acedecision():
-    global playerscore
-    if playerscore + (11 * acecount) < 21:
-        playerscore += 11 * acecount
-        return
-    elif playerscore + (11 * (acecount- 1)) + 1*1 < 21:
-        playerscore += (11 * (acecount- 1)) + 1*1
-        return
-    elif playerscore + (11 * (acecount- 2)) + 1*2 < 21:
-        playerscore += (11 * (acecount- 2)) + 1*2
-        return
-    elif playerscore + (11 * (acecount- 3)) + 1*3 < 21:
-        playerscore += (11 * (acecount- 3)) + 1*3
-        return
+            
+    winningplayer = max(finallist)
+    playerpos = finallist.index(winningplayer)
+    if finallist.count(winningplayer) > 1:
+        tielist = [i for i,j in enumerate(finallist) if j == winningplayer]
+        tietext = str()
+        num = 0
+        for i in tielist:
+            if num == 0:
+                tietext += (str(i+1))
+                num += 1
+            else:
+                tietext += str(", " + str(i+1))
+        mastercanvas.create_text(150,(350 + (playernum* 25)),text=("Player "+ tietext+" have tied at "+str(int(max(finallist)))+"."))
+        for i in tielist:
+            winnericon = tk.Label(masterwindow)
+            banner = Image.open(str("assets/winner.png"))
+            banner = banner.resize((120,120))
+            banner = ImageTk.PhotoImage(banner)
+            winnericon.photo = banner
+            winnerpic = mastercanvas.create_image(65 + (165*int(i) + 12.5*(len(cardcounter[i])) - 1) ,285, image = banner)
+            mastercanvas.update()
+        
     else:
-        playerscore += (1 * 4)
-        return
-                
+        print(playerposition)
+        mastercanvas.create_text(150,(350 + (playernum* 25)),justify = "left",text=("Player "+ str(int(playerpos+1))+" WINS with a score of "+str(int(winningplayer))))
+        winnericon = tk.Label(masterwindow)
+        banner = Image.open(str("assets/winner.png"))
+        banner = banner.resize((120,120))
+        banner = ImageTk.PhotoImage(banner)
+        winnericon.photo = banner
+        winnerpic = mastercanvas.create_image(65 + (165*int(playerpos)) + 12.5*(len(cardcounter[playerpos]) - 1) ,285, image = banner)
+        mastercanvas.update()
+       
 
-    
+def acedecision(playerscore, acecount):
+    if playerscore + (11 * acecount) <= 21:
+        print("choice 1")
+        return playerscore + (11 * acecount)
+
+    elif playerscore + (11 * ((abs(acecount- 1) + acecount-1)/2)) + 1*1  <= 21:
+        print("choice 2")
+        return playerscore + (11 * ((abs(acecount- 1) + acecount-1)/2)) + 1*1
+
+    elif playerscore + (11 * ((abs(acecount- 2) + acecount-2)/2)) + 1*2 <= 21:
+        print("choice 3")
+        return playerscore + (11 * ((abs(acecount- 2) + acecount-2)/2)) + 1*2
+
+    elif playerscore + (11 * ((abs(acecount- 3) + acecount-3)/2)) + 1*3 <= 21:
+        print("choice 4")
+        return playerscore + (11 * ((abs(acecount- 4) + acecount-3)/2)) + 1*3
+
+    else:
+        print("choice 5")
+        return playerscore + (1 * acecount)
+                
 setup()
 gameround()
 
-####all that is needed now is to exclude players who are bust from the scorescounter list (maybe make a seperate list for people who arent bust#
-#so the winner printed is in fact the winner#
-
-#pretty much done after that :))) well done#
 
 
 
